e7a92297cddccd9e358b2b2955ecddfb87dc64b0
joshua <josh@joshuadennis.ca>
2019-01-14 23:00:17 +0000

    modulized step 4

---
+++ getHTML.js
@@ -0,0 +1,28 @@
+https = require('https');
+function getHTML (options, callback) {
+
+  var dataReceived = '';
+
+
+  https.get(options, function (response) {
+
+    response.on('data', function(data) {
+      dataReceived += data;
+    });
+  
+    response.on('end', function() {
+      callback(dataReceived);
+
+});
+  });
+}
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
