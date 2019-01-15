a40342eb44938cec013b8269da35113792c15020
joshua <josh@joshuadennis.ca>
2019-01-15 00:10:55 +0000

    created module: step 5

---
+++ http-functions.js
@@ -0,0 +1,27 @@
+module.exports = function getHTML (options, callback) {
+  https = require('https');
+  var dataReceived = '';
+
+
+  https.get(options, function (response) {
+
+    response.on('data', function(data) {
+      dataReceived += data;
+    });
+  
+    response.on('end', function() {
+      callback(dataReceived);
+
+});
+  });
+
+// function printHTML (html) {
+  // console.log(html);
+// }
+
+// var requestOptions = {
+  // host: 'sytantris.github.io',
+  // path: '/http-examples/step4.html'
+// };
+};
+// getHTML(options, callback);
